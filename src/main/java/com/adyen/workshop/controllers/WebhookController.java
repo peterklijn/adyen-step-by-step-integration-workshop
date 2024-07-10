aa349534290b7b3cd65b9d136a1072b85a9179ae
Peter Klijn <pjmklijn@gmail.com>
2024-07-10 12:24:33 +0000

    Step 16

--- src/main/java/com/adyen/workshop/controllers/WebhookController.java
+++ src/main/java/com/adyen/workshop/controllers/WebhookController.java
@@ -5,10 +5,12 @@ import com.adyen.model.notification.NotificationRequestItem;
 import com.adyen.util.HMACValidator;
 import com.adyen.workshop.configurations.ApplicationConfiguration;
 import org.apache.coyote.Response;
+import org.apache.hc.core5.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,6 +40,33 @@ public class WebhookController {
     @PostMapping("/webhooks")
     public ResponseEntity<String> webhooks(@RequestBody String json) throws Exception {
         // Step 16
-        return null;
+        log.info("Received webhook: {}", json);
+        try {
+            var notificationRequest = NotificationRequest.fromJson(json);
+            var notificationRequestItem = notificationRequest.getNotificationItems().stream().findFirst();
+
+            var item = notificationRequestItem.get();
+            if (!hmacValidator.validateHMAC(item, applicationConfiguration.getAdyenHmacKey())) {
+                log.warn("Could not validate HMAC signature for incoming webhook message: {}", item);
+                return ResponseEntity.unprocessableEntity().build();
+            }
+
+            // Success, log it for now
+            log.info("""
+                    Received webhook with event {} :\s
+                    Merchant Reference: {}
+                    Alias : {}
+                    PSP reference : {}""",
+            item.getEventCode(),
+            item.getMerchantReference(),
+            item.getAdditionalData().get("alias"),
+            item.getPspReference());
+
+        } catch (Exception e) {
+            log.error("Exception while handling webhook", e);
+            ResponseEntity.internalServerError();
+        }
+
+        return ResponseEntity.ok("");
     }
 }
